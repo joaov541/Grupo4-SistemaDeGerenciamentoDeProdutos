8e845c7c4f71fca00c89242a238da1596c3455ab
JOÃO VITOR DO NASCIMENTO SANTOS <54198904880@info.local>
2026-03-26 16:26:44 -0300

    Inicializado a conrtrução dos repositorios e interfaces

--- GerenciamentoDeProdutos/GerenciamentoDeProdutos.WebAPI/Models/Categoria.cs
+++ GerenciamentoDeProdutos/GerenciamentoDeProdutos.WebAPI/Models/Categoria.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GerenciamentoDeProdutos.WebAPI.Models;
 
-public partial class Categorium
+public partial class Categoria
 {
     [Key]
     public Guid IdCategoria { get; set; }
